e49da920054d83293fadd1932cacbf42c90ec7cb
Peter Knipp <peteraknipp@gmail.com>
2024-09-02 17:32:49 -0400

    Add a colon.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   # root 'welcome#index'
-  get '/', to 'welcome#index'
+  get '/', to: 'welcome#index'
   get 'welcome/:id', to: 'welcome#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
